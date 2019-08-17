14710495bf81a353df8693132232837da3a4413e
Mgrdich <mgotm13@gmail.com>
2019-08-18 00:17:32 +0400

    importing working sass without the watching part

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import {StoreProvider} from "./Store/Store";
 import "bootstrap/dist/css/bootstrap.css";
