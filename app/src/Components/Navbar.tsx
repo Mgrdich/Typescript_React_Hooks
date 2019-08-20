cab07d097dfab2026c4f1fa79b76433efcd12aa4
Mgrdich <mgotm13@gmail.com>
2019-08-20 22:55:02 +0400

    adding histroy partially

--- app/src/Components/Navbar.tsx
+++ app/src/Components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import {createBrowserHistory} from "history";
 
-const Navbar = () => {
+export const Navbar = (props: any) => {
+    console.log(createBrowserHistory());
     return (
         <>
-            <h1>Rick and Morty</h1>
+            <h1 className="main_header">Rick and Morty</h1>
             <NavLink className="nav-link" to="/Favourites">FAV</NavLink>
         </>
     );
---
+++ app/src/Components/error.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = (props:any) => {
+    console.log(props);
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Error;
