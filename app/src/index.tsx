02b10e96b8de244e653f76b9fa0181282d96dfad
Mgrdich <mgotm13@gmail.com>
2019-08-20 22:13:41 +0400

    applying the background image and fixing some styling issue

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import {StoreProvider} from "./Store/Store";
 import "bootstrap/dist/css/bootstrap.css";
 import {BrowserRouter} from "react-router-dom";
+import "../src/Style/style.css"
 
 
 
