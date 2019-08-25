672c365f5200d4ddbcb2953cab9e20a450b5b5ec
Mgrdich <mgotm13@gmail.com>
2019-08-25 16:29:40 +0400

    adding the logo + some vector files

--- app/src/Components/Navbar.tsx
+++ app/src/Components/Navbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import {createBrowserHistory} from "history";
+import logo from "../Style/src/logo.svg"
 
 export const Navbar = (props: any) => {
     console.log(createBrowserHistory());
     return (
         <div className="myNavbar">
-            <h1 className="main_header">Rick and Morty</h1>
+            <div className="main-header-image">
+                <img src={logo} alt=""/>
+            </div>
             <NavLink className="nav-link" to="/Favourites">FAV</NavLink>
         </div>
     );
